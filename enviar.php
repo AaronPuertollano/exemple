7edd27c4308e33d7ea9c2d085ec107930b37272d
aaron <apuertollanoc@esliceu.net>
2023-04-21 18:58:06 +0200

    inici

---
+++ enviar.php
@@ -0,0 +1,8 @@
+<?php
+  echo "hola, ".$_POST["nom"];
+  $i = 0;
+  while ($i <5) {
+    echo $i."<br>";
+    $i++;
+  }
+?>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form action="enviar.php" method="get">
+  Nom:  <input type="text" name="nom" id="nom"><br>
+  Contrasenya:  <input type="text" name="cont" id="cont"><br>
+  <input type="submit" value="enviar"><br>
+</form>
+
+</body>
+</html>
